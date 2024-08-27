688348bf3432c03883f738bace211322ba69329b
nikita <wot_98_98@mail.ru>
2024-08-27 13:03:51 +0600

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import logging
 
 import traceback
 
+from Src.Colors import *
 from Src.Login import hamster_client
 from Src.Menu import handle_main_menu_choice, main_menu
-from Src.utils import WHITE, RESET, CYAN, line_before, banner
-from Src.Settings import load_settings
+from Src.utils import line_before, banner
+from Src.Settings import load_setting
 
 from dotenv import load_dotenv
 
@@ -13,16 +14,14 @@ load_dotenv()
 
 logging.basicConfig(format=f"{WHITE}%(asctime)s - %(name)s - %(levelname)s |  %(message)s  | %(filename)s - %(funcName)s() - %(lineno)d{RESET}", level=logging.ERROR)
 
-
 if __name__ == '__main__':
     banner()
     hamster_client().login()
     main_menu()
 
     try:
-        settings = load_settings()
         while True:
-            if settings['hamster_token']:
+            if load_setting('hamster_token'):
                 choice = input(f"\nВыберите действие\n{CYAN}(#/1/2/3/4/5/6/$/+/m/0):{RESET} ")
             else:
                 choice = input(f"\nВыберите действие\n{CYAN}(6/m/0):{RESET} ")
