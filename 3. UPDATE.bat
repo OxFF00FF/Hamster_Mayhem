e2aee3ae04a97e7a618787348ea3baea577271e5
nikita <wot_98_98@mail.ru>
2024-08-29 12:27:12 +0600

    Добавлен bat файл для обновления кода

--- 3. UPDATE.bat
+++ 3. UPDATE.bat
@@ -5,11 +5,11 @@ SET REMOTE=https://github.com/OxFF00FF/Hamster_Mayhem.git
 if not exist ".git" (
     git init
     git add .
-    git remote add origin %REMOTE_URL%
+    git remote add origin %REMOTE%
     git fetch
     git checkout master
     git reset --hard
-    git pull %REMOTE_URL% master
+    git pull %REMOTE% master
 )
 
 echo Getting updates...
