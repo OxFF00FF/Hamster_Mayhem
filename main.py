c268d4c9b65875a1544730a88c48ff8254a6931f
nikita <wot_98_98@mail.ru>
2024-09-02 12:51:13 +0600

    translate main.py to en

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from Src.Colors import *
 from Src.db_SQlite import ConfigDB
 from Src.Handlers import handle_main_menu_choice
 from Src.Login import hamster_client
-from Src.Menu import main_menu
-from Src.utils import banner, line_after
+from Src.Menu import main_menu, main_menu_not_logged
+from Src.utils import banner, line_after, localized_text
 
 logging.basicConfig(format=f"{WHITE}%(asctime)s - %(name)s - %(levelname)s |  %(message)s  | %(filename)s - %(funcName)s() - %(lineno)d{RESET}", level=logging.ERROR)
 
@@ -18,16 +18,18 @@ lang = config.lang
 if __name__ == '__main__':
     # banner()
     hamster_client().login()
-    main_menu()
 
     try:
         while True:
             if config.hamster_token:
-                choice = input(f"{DARK_GRAY}Выберите действие:\n{CYAN}(#/1/2/3/4/5/6/a/$/+/s/m/0):{RESET} ")
+                main_menu()
+                choice = input(f"{DARK_GRAY}{localized_text('choose_action')}:\n{CYAN}(#/1/2/3/4/5/6/a/$/+/s/m/0):{RESET} ")
             else:
-                choice = input(f"{DARK_GRAY}Выберите действие:\n{CYAN}(6/m/0):{RESET} ")
+                main_menu_not_logged()
+                choice = input(f"{DARK_GRAY}{localized_text('choose_action')}:\n{CYAN}(6/m/0):{RESET} ")
+
             handle_main_menu_choice(choice)
             line_after()
 
     except Exception as e:
-        logging.error(f"🚫  Произошла ошибка: {e}\n{traceback.format_exc()}\n")
+        logging.error(f"{e}\n{traceback.format_exc()}\n")
