938fe6b38eeb5950dcb15a5212e4302e3c981275
nikita <wot_98_98@mail.ru>
2024-09-02 11:22:45 +0600

    изменено получение и запись в бд в Generators.py

--- Src/Generators.py
+++ Src/Generators.py
@@ -1,11 +1,12 @@
 import asyncio
 import logging
 
+from Src.Colors import *
+from Src.db_SQlite import ConfigDB
 from Src.Login import hamster_client
-from Src.Settings import load_settings, load_setting
 from Src.utils import get_games_data
 
-settings = load_settings()
+config = ConfigDB()
 
 
 def generate_promocodes(prefix='', apply_promo=False):
@@ -18,10 +19,7 @@ def generate_promocodes(prefix='', apply_promo=False):
         logging.error(f"\nКоличество должно быть числом больше 0")
 
     try:
-        send_to_group = load_setting('send_to_group')
-        save_to_file = load_setting('save_to_file')
-        spinner = load_setting('spinner')
-        asyncio.run(hamster_client().get_promocodes(int(count), send_to_group, apply_promo, prefix, save_to_file, spinner))
+        asyncio.run(hamster_client().get_promocodes(int(count), config.send_to_group, apply_promo, prefix, config.save_to_file, config.spinner))
 
     except Exception as e:
         logging.error(e)
@@ -32,10 +30,10 @@ def generate_promocodes(prefix='', apply_promo=False):
 
 
 def generate_for_game(prefix):
-    choice_text = "\nХотите применить промокоды после получения?\nY(да) / Enter(Нет): "
-    if settings.get('hamster_token'):
-        if settings.get('apply_promo'):
-            generate_promocodes(prefix=prefix, apply_promo=settings['apply_promo'])
+    choice_text = f"\n{DARK_GRAY}Хотите применить промокоды после получения?{CYAN}\nY(да) / Enter(Нет): "
+    if config.hamster_token:
+        if config.apply_promo:
+            generate_promocodes(prefix=prefix, apply_promo=config.apply_promo)
         else:
             choice = input(choice_text).lower()
             if choice == 'y':
@@ -51,18 +49,18 @@ def generate_for_game(prefix):
 async def genetare_for_all_games():
     apps = get_games_data()['apps']
 
-    if settings['hamster_token']:
+    if config.hamster_token:
         choice = input(f"\nХотите применить промокоды после получения?\nY(да) / Enter(Нет): ")
         apply_promo = str(choice.lower()) == 'y'.lower()
 
     count = input(f"\nКоличество промокодов для всех игр Enter(по умолчанию 1): ")
     if count == '':
         count = 1
-        print("\nКоличество промокодов не предоставлено. Генерируется 1 по умолчанию")
+        print("\nКоличество промокодов не предоставлено.\nГенерируется 1 по умолчанию")
 
     if int(count) <= 0:
         logging.error(f"\nКоличество должно быть числом больше 0")
         exit(1)
 
-    tasks = [hamster_client().get_promocodes(int(count), settings['send_to_group'], apply_promo, app["prefix"], settings['save_to_file']) for app in apps]
+    tasks = [hamster_client().get_promocodes(int(count), config.send_to_group, apply_promo, app["prefix"], config.save_to_file) for app in apps]
     await asyncio.gather(*tasks)
