29e8896ff73785cda062993004e10df154edf7c8
nikita <wot_98_98@mail.ru>
2024-09-03 09:22:01 +0600

    Update

--- INSTALL.bat
+++ INSTALL.bat
@@ -1,20 +1,29 @@
 @echo off
 
-echo Creating virtual environment...
-python -m venv venv
+if not exist "venv" (
+    echo Creating virtual environment...
+    python -m venv venv
 
-echo Activating virtual environment...
-call venv\Scripts\activate
+    echo Activating virtual environment...
+    call venv\Scripts\activate
+    echo.
 
-echo Installing dependencies...
-pip install -r requirements.txt
+    echo Installing dependencies...
+    pip install -r requirements.txt
+    echo.
+)
+
 
 if not exist ".env" (
     echo Copying .env.example to .env...
     copy .env.example .env
+    echo.
 )
 
-
+echo Required dependencies installed
+echo.
 echo Please edit the .env file to add your HAMSTER_TOKEN
-
+echo.
 pause
+
+start .env
