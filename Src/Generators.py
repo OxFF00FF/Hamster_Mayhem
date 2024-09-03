aaaca8c7575b22ff21805cb63d05bb8ab957c2c1
nikita <wot_98_98@mail.ru>
2024-09-03 09:44:03 +0600

    Add translation

--- Src/Generators.py
+++ Src/Generators.py
@@ -5,45 +5,45 @@ import asyncio
 from Src.Colors import *
 from Src.db_SQlite import ConfigDB
 from Src.Login import hamster_client
-from Src.utils import get_games_data
+from Src.utils import get_games_data, localized_text
 
 config = ConfigDB()
-lang = config.lang
+
+choice_text = f"\n{DARK_GRAY}{localized_text('apply_promo_after_generate')}{CYAN}\n{localized_text('yes_enter')}: ".lower()
 
 
 def generate_promocodes(prefix='', apply_promo=False):
-    count = input(f"\nКакое количество промокодов генерировать?\nEnter(по умолчанию 1): ")
+    count = input(f"\n{DARK_GRAY}{localized_text('count_promocodes_to_generate')}{CYAN}\n{localized_text('enter_one')}: {WHITE}")
     if count == '':
         count = 1
-        print(f"⚠️  Количество промокодов не указано. Генерируется 1 по умолчанию")
+        print(localized_text('count_not_specified'))
 
     if int(count) <= 0:
-        logging.error(f"\nКоличество должно быть числом больше 0")
+        print(localized_text('error_count_must_great_0'))
 
     try:
         asyncio.run(hamster_client().get_promocodes(int(count), config.send_to_group, apply_promo, prefix, config.save_to_file, config.spinner))
 
     except Exception as e:
         logging.error(e)
-        print(f"🚫  Произошла ошибка во время генерации. Попробуйте снова, если ошибки прололжаться, то попробуйте позже.")
+        print(f"🚫  {localized_text('error_generate_failed')}")
 
     finally:
         pass
 
 
 def generate_for_game(prefix):
-    choice_text = f"\n{DARK_GRAY}Хотите применить промокоды после получения?{CYAN}\nY(да) / Enter(Нет): "
     if config.hamster_token:
         if config.apply_promo:
             generate_promocodes(prefix=prefix, apply_promo=config.apply_promo)
         else:
-            choice = input(choice_text).lower()
+            choice = input(choice_text)
             if choice == 'y':
                 generate_promocodes(prefix=prefix, apply_promo=True)
             elif choice == '':
                 generate_promocodes(prefix=prefix)
             else:
-                print("Такой опции нет")
+                print(localized_text('no_such_option'))
     else:
         generate_promocodes(prefix=prefix)
 
@@ -52,16 +52,16 @@ async def genetare_for_all_games():
     apps = get_games_data()['apps']
 
     if config.hamster_token:
-        choice = input(f"\nХотите применить промокоды после получения?\nY(да) / Enter(Нет): ")
+        choice = input(choice_text)
         apply_promo = str(choice.lower()) == 'y'.lower()
 
-    count = input(f"\nКоличество промокодов для всех игр Enter(по умолчанию 1): ")
+    count = input(localized_text('count_promocodes_to_generate_all_games'))
     if count == '':
         count = 1
-        print("\nКоличество промокодов не предоставлено.\nГенерируется 1 по умолчанию")
+        print(localized_text('count_not_specified'))
 
     if int(count) <= 0:
-        logging.error(f"\nКоличество должно быть числом больше 0")
+        print(localized_text('error_count_must_great_0'))
         exit(1)
 
     tasks = [hamster_client().get_promocodes(int(count), config.send_to_group, apply_promo, app["prefix"], config.save_to_file) for app in apps]
