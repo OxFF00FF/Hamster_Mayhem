6481c1286d21b93b2480ac6355a3ae9f9f4b8304
nikita <wot_98_98@mail.ru>
2024-09-09 17:41:07 +0600

    Update

--- main_docker.py
+++ main_docker.py
@@ -1,9 +1,49 @@
+import threading
+import time
+from random import randint
+
 from dotenv import load_dotenv
 
 from Src.Login import hamster_client
+from Src.utils import remain_time, line_before, line_after, countdown_timer
+
 load_dotenv()
-
-# cooldowns = hamster_client().get_cooldowns()
-
 hamster_client().login()
-hamster_client().send_balance_to_group(chat_id=-1002155132619)
+
+
+def random_delay():
+    return randint(1, 3600)
+
+
+def process_taps():
+    taps = hamster_client().get_cooldowns()['taps']
+    while True:
+        hamster_client().complete_taps()
+        time.sleep(taps['remain'] + random_delay())
+
+
+def process_cipher():
+    cipher = hamster_client().get_cooldowns()['cipher']
+    while True:
+        hamster_client().complete_daily_chipher()
+        time.sleep(cipher['remain'] + random_delay())
+
+
+def process_tasks():
+    tasks = hamster_client().get_cooldowns()['tasks']
+    while True:
+        hamster_client().complete_daily_chipher()
+        time.sleep(tasks['remain'] + random_delay())
+
+
+threads = [
+    threading.Thread(target=process_taps),
+    threading.Thread(target=process_cipher),
+    threading.Thread(target=process_tasks),
+]
+
+for thread in threads:
+    thread.start()
+
+for thread in threads:
+    thread.join()
