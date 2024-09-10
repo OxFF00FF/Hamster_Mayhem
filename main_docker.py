a09ac4929a4422c7c12cb6fb57e9f1cbce87b2a0
nikita <wot_98_98@mail.ru>
2024-09-10 12:59:07 +0600

    add process_minigame_tiles and process_minigame_candles

--- main_docker.py
+++ main_docker.py
@@ -1,47 +1,93 @@
 import os
 import threading
 import time
+from datetime import datetime
 from random import randint
 
 from dotenv import load_dotenv
 
 from Src.Login import hamster_client
-from Src.utils import remain_time, line_before, line_after, countdown_timer
+from Src.utils import line_before, line_after, remain_time
 
 load_dotenv()
-hamster_client().login()
+user = hamster_client().login().split()[-1].strip('(').strip(')')
+
+print_lock = threading.Lock()
 
 
 def random_delay():
-    return randint(1, 3600)
+    return randint(1000, 2000)
 
 
 def process_taps():
-    taps = hamster_client().get_cooldowns()['taps']
     while True:
-        hamster_client().complete_taps()
-        time.sleep(taps['remain'] + random_delay())
+        with print_lock:
+            line_before(blank_line=False)
+            print(f"⚙️  {datetime.now()} · {user}")
+            remain = hamster_client().complete_taps()
+            print(f"⏳   Следующие тапы через: {remain_time(remain)}")
+            line_after(blank_line=False)
+        time.sleep(remain + random_delay())
 
 
 def process_cipher():
-    cipher = hamster_client().get_cooldowns()['cipher']
     while True:
-        hamster_client().complete_daily_chipher()
-        time.sleep(cipher['remain'] + random_delay())
+        with print_lock:
+            line_before(blank_line=False)
+            print(f"⚙️  {datetime.now()} · {user}")
+            remain = hamster_client().complete_daily_chipher()
+            print(f"⏳   Следующий шифр через: {remain_time(remain)}")
+            line_after(blank_line=False)
+        time.sleep(remain + random_delay())
 
 
 def process_tasks():
-    tasks = hamster_client().get_cooldowns()['tasks']
     while True:
-        hamster_client().complete_daily_chipher()
-        time.sleep(tasks['remain'] + random_delay())
+        with print_lock:
+            line_before(blank_line=False)
+            print(f"⚙️  {datetime.now()} · {user}")
+            remain = hamster_client().complete_daily_tasks()
+            print(f"⏳   Следующие задания через: {remain_time(remain)}")
+            line_after(blank_line=False)
+        time.sleep(remain + random_delay())
 
 
 def process_balance():
-    hamster_client().send_balance_to_group()
+    while True:
+        with print_lock:
+            line_before(blank_line=False)
+            print(f"⚙️  {datetime.now()} · {user}")
+            remain = hamster_client().send_balance_to_group(update_time_sec=5000, chat_id=int(os.getenv('BOT_LOGS_GROUP_ID')))
+            print(f"⏳   Следующий баланс через: {remain_time(remain)}")
+            line_after(blank_line=False)
+        time.sleep(remain + random_delay())
+
+
+def process_minigame_tiles():
+    while True:
+        with print_lock:
+            line_before(blank_line=False)
+            print(f"⚙️  {datetime.now()} · {user}")
+            remain = hamster_client().complete_daily_minigame('tiles')
+            print(f"⏳   Следующая игра Tiles через: {remain_time(remain)}")
+            line_after(blank_line=False)
+        time.sleep(remain + random_delay())
+
+
+def process_minigame_candles():
+    while True:
+        with print_lock:
+            line_before(blank_line=False)
+            print(f"⚙️  {datetime.now()} · {user}")
+            remain = hamster_client().complete_daily_minigame('candles')
+            print(f"⏳   Следующая игра Candles через: {remain_time(remain)}")
+            line_after(blank_line=False)
+        time.sleep(remain + random_delay())
 
 
 threads = [
+    threading.Thread(target=process_minigame_tiles),
+    threading.Thread(target=process_minigame_candles),
     threading.Thread(target=process_balance),
     threading.Thread(target=process_taps),
     threading.Thread(target=process_cipher),
