2517f2b5cd82a1dffe55ccceda969ecd739da335
nikita <wot_98_98@mail.ru>
2024-09-11 14:01:15 +0600

    Fix

--- Src/Handlers.py
+++ Src/Handlers.py
@@ -136,7 +136,7 @@ def handle_playground_menu_choice():
     while True:
         playground_menu()
         # choice = input(f"{DARK_GRAY}{localized_text('choose_action')}\n{CYAN}▶️  (1/2/3/4/5/6/7/8/9/*/</0): {RESET}")
-        choice = input(kali(f"{'/'.join(games)}/</0"))
+        choice = input(kali(f"{'/'.join(games)}/</0", '~/Playground'))
 
         line_before()
 
@@ -172,7 +172,7 @@ def handle_minigames_choice():
         minigames_menu()
         choices = [str(i + 1) for i in range(len(minigames))]
         # choice = input(f"{DARK_GRAY}{localized_text('choose_action')}\n{CYAN}▶️  ({'/'.join(choices)}/</0): {RESET}")
-        choice = input(kali(f"{'/'.join(choices)}/</0"))
+        choice = input(kali(f"{'/'.join(choices)}/</0", '~/Minigames'))
         line_before()
 
         if choice in choices:
@@ -200,7 +200,7 @@ def handle_settings_menu_choice():
     while True:
         settings_menu()
         # choice = input(f"{DARK_GRAY}{localized_text('choose_action')}\n{CYAN}▶️  (1/2/3/4/5/</0): {RESET}")
-        choice = input(kali('1/2/3/4/5/</0'))
+        choice = input(kali('1/2/3/4/5/</0', '~/Settings'))
         line_before()
 
         if choice == '1':
