72d3a984e59f92a68c14644df3827a1bf1af34c8
nikita <wot_98_98@mail.ru>
2024-09-11 11:45:41 +0600

    Fix

--- Src/Login.py
+++ Src/Login.py
@@ -6,8 +6,11 @@ from Src.Hamster import HamsterKombatClicker
 config = ConfigDB()
 
 
-def hamster_client():
+def hamster_client(token=None):
     try:
+        if token is not None:
+            config.account = token
+
         HAMSTER_TOKEN = os.getenv(config.account)
         return HamsterKombatClicker(HAMSTER_TOKEN)
 
