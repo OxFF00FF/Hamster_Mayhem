34d97a960d1178712362ea94e78a197794e85881
nikita <wot_98_98@mail.ru>
2024-09-20 17:57:19 +0600

    Change logger

--- Src/db_SQlite.py
+++ Src/db_SQlite.py
@@ -6,7 +6,20 @@ from dotenv import load_dotenv
 
 from Src.Colors import *
 
-logging.basicConfig(format=f"{WHITE}%(asctime)s - %(name)s - %(levelname)s |  %(message)s  | %(filename)s - %(funcName)s() - %(lineno)d{WHITE}", level=logging.ERROR)
+formatter = logging.Formatter(f"{WHITE}%(asctime)s - %(name)s - %(levelname)s |  %(message)s  | %(filename)s - %(funcName)s() - %(lineno)d{WHITE}")
+logging.basicConfig(format=formatter._fmt, level=logging.ERROR)
+
+sqlalchemy_logger = logging.getLogger('sqlalchemy.engine')
+sqlalchemy_logger.setLevel(logging.ERROR)
+
+sqlalchemy_logger.propagate = False
+
+handler = logging.StreamHandler()
+handler.setFormatter(formatter)
+
+sqlalchemy_logger.handlers = []
+sqlalchemy_logger.addHandler(handler)
+
 load_dotenv()
 
 
