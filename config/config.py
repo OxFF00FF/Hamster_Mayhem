8dbdbfff189a55cbc95ebf8365cac54256bd6ddb
OxFF00FF <Wot-98_98@mail.ru>
2024-09-21 17:34:17 +0600

    Fix

--- config/config.py
+++ config/config.py
@@ -4,16 +4,16 @@ from typing import Optional
 
 class Config(BaseSettings):
     HAMSTER_TOKEN_1: str
-    HAMSTER_TOKEN_2: Optional[str]
-    HAMSTER_TOKEN_3: Optional[str]
+    HAMSTER_TOKEN_2: Optional[str] = None
+    HAMSTER_TOKEN_3: Optional[str] = None
 
-    TELEGRAM_BOT_TOKEN: Optional[int]
+    TELEGRAM_BOT_TOKEN: Optional[str]
     CHAT_ID: Optional[int]
     GROUP_URL: Optional[str]
-    BOT_LOGS_GROUP_ID: Optional[int]
+    BOT_LOGS_GROUP_ID: Optional[int] = None
 
-    TELEGRAM_API_ID: Optional[str]
-    TELEGRAM_API_HASH: Optional[str]
+    TELEGRAM_API_ID: Optional[str] = None
+    TELEGRAM_API_HASH: Optional[str] = None
 
     @property
     def DB_URL_sqlite(self):
