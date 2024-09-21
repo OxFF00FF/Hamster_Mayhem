e3bbcf696e572e8a763fafe7450555bd7d32c4ce
OxFF00FF <Wot-98_98@mail.ru>
2024-09-21 16:00:45 +0600

    Fix

--- config/config.py
+++ config/config.py
@@ -1,18 +1,19 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from typing import Optional
 
 
 class Config(BaseSettings):
     HAMSTER_TOKEN_1: str
-    HAMSTER_TOKEN_2: str
-    HAMSTER_TOKEN_3: str
+    HAMSTER_TOKEN_2: Optional[str]
+    HAMSTER_TOKEN_3: Optional[str]
 
-    TELEGRAM_BOT_TOKEN: str
-    CHAT_ID: int
-    GROUP_URL: str
-    BOT_LOGS_GROUP_ID: int
+    TELEGRAM_BOT_TOKEN: Optional[int]
+    CHAT_ID: Optional[int]
+    GROUP_URL: Optional[str]
+    BOT_LOGS_GROUP_ID: Optional[int]
 
-    TELEGRAM_API_ID: str
-    TELEGRAM_API_HASH: str
+    TELEGRAM_API_ID: Optional[str]
+    TELEGRAM_API_HASH: Optional[str]
 
     @property
     def DB_URL_sqlite(self):
