7e305a9a0f9ee15d6a9e7587fcb0d8aca63bea98
Nikita <wot_98_98@mail.ru>
2024-09-22 09:48:46 +0600

    Update config.py

--- config/config.py
+++ config/config.py
@@ -7,9 +7,9 @@ class Config(BaseSettings):
     HAMSTER_TOKEN_2: Optional[str] = None
     HAMSTER_TOKEN_3: Optional[str] = None
 
-    TELEGRAM_BOT_TOKEN: Optional[str]
-    CHAT_ID: Optional[int]
-    GROUP_URL: Optional[str]
+    TELEGRAM_BOT_TOKEN: Optional[str] = None
+    CHAT_ID: Optional[int] = None
+    GROUP_URL: Optional[str] = None
     BOT_LOGS_GROUP_ID: Optional[int] = None
 
     TELEGRAM_API_ID: Optional[str] = None
